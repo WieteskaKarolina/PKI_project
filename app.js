7e4551c19cc8b5cf6c111430cdebefaec4f014fd
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-05-15 19:00:23 +0200

    add table views

--- app.js
+++ app.js
@@ -1,28 +1,19 @@
 const express = require('express');
-const pgp = require('pg-promise')();
 const app = express();
 const port = 3000;
+
+const pgp = require('pg-promise')();
 const db = pgp(process.env.DATABASE_URL);
 
-// app.use(express.static('views'));
-// app.set('view engine', 'ejs');
-
-app.get('/databaseName', (req, res) => {
-  db.one('SELECT current_database()')
-    .then(data => {
-      res.send(data.current_database);
-    })
-    .catch(error => {
-      console.error('Błąd pobierania nazwy bazy danych:', error);
-      res.status(500).send('Wystąpił błąd serwera');
-    });
-});
+// Konfiguracja EJS
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 app.get('/tableList', (req, res) => {
   db.any("SELECT table_name FROM information_schema.tables WHERE table_schema='public' AND table_type='BASE TABLE'")
     .then(data => {
       const tableList = data.map(row => row.table_name);
-      res.send(tableList);
+      res.render('tableList', { tableList });
     })
     .catch(error => {
       console.error('Błąd pobierania listy tabel:', error);
@@ -35,7 +26,7 @@ app.get('/tableContent', (req, res) => {
 
   db.any(`SELECT * FROM ${tableName}`)
     .then(data => {
-      res.send(data);
+      res.render('tableContent', { tableName, tableData: data });
     })
     .catch(error => {
       console.error('Błąd pobierania zawartości tabeli:', error);
@@ -44,10 +35,9 @@ app.get('/tableContent', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/home.html');
+  res.sendFile(__dirname + '/home.html');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
-
