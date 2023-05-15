f4927153a8e6084538725d9f9f5d331de18058a0
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-05-15 12:26:48 +0200

    add db connection

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const pgp = require('pg-promise')();
+const db = pgp(process.env.DATABASE_URL);
+
+db.one('SELECT 1')
+  .then(data => {
+    console.log('Połączenie z bazą danych jest poprawne');
+  })
+  .catch(error => {
+    console.error('Błąd połączenia z bazą danych:', error);
+  });
+  
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 })
