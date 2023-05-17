a6826ab44b56c382b162402916a15f68105ca85f
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-05-17 15:31:13 +0200

    change app to router in login

--- routers/login.js
+++ routers/login.js
@@ -5,11 +5,11 @@ const router = express.Router();
 const adminUsername = "admin";
 const adminPassword = "admin132";
 
-app.get('/login', (req, res) => {
+router.get('/login', (req, res) => {
     res.render('login');
 });
 
-app.post("/", (req, res) => {
+router.post("/", (req, res) => {
   const { username, password } = req.body;
   if (username === adminUsername && password === adminPassword) {
     req.session.isAdmin = true; 
