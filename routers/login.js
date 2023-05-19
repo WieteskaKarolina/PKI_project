2328b39a9d8c6ccf7e22c9c8be7c68cab39a7718
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-05-19 21:11:35 +0200

    change hash value

--- routers/login.js
+++ routers/login.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcrypt');
 
 const adminUsername = "admin";
-const adminPasswordHash = "$2b$10$qfIovW8dyoZiYAB2aEd1P.6H6mflvcAlTMRiEdA2ITBamNNsStL6e"; // Hashed version of "admin132"
+const adminPasswordHash = "$2b$10$qfIovW8dyoZiYAB2aEd1P.6H6mflvcAlTMRiEdA2ITBamNNsStL6e";
 
 router.get('/', (req, res) => {
   res.render('login');
