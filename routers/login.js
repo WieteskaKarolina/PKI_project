95bf4809309f06b3f795403387996ccf284107ae
WieteskaKarolina <wieteskakarolina02@gmail.com>
2023-05-19 20:57:47 +0200

    add session and bcrypt

--- routers/login.js
+++ routers/login.js
@@ -1,8 +1,9 @@
-var express = require('express');
+const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
 
 const adminUsername = "admin";
-const adminPassword = "admin132";
+const adminPasswordHash = "$2b$10$qfIovW8dyoZiYAB2aEd1P.6H6mflvcAlTMRiEdA2ITBamNNsStL6e"; // Hashed version of "admin132"
 
 router.get('/', (req, res) => {
   res.render('login');
@@ -10,9 +11,8 @@ router.get('/', (req, res) => {
 
 router.post("/", (req, res) => {
   const { username, password } = req.body;
-  if (username === adminUsername && password === adminPassword) {
-    const isAdmin = true;
-    res.cookie('isAdmin', isAdmin);
+  if (username === adminUsername && bcrypt.compareSync(password, adminPasswordHash)) {
+    req.session.isAdmin = true;
     res.redirect('/');
   } else {
     res.redirect('/login?invalid=true');
